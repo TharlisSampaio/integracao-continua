f12d8250713490145f90e83b0cf51a2fc1ee73a6
TharlisSampaio <100005064+TharlisSampaio@users.noreply.github.com>
2022-11-21 18:02:53 -0500

    Update FiltroCors.java

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -17,9 +17,9 @@ public class FiltroCors {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(
-            Arrays.asList(
-                "http://127.0.0.1:5500",
-                "https://localhost:4200")
+            Arrays.asList("https://sgcm-tharlis-front-production.up.railway.app/")
+   //             "http://127.0.0.1:5500",
+   //             "https://localhost:4200")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
